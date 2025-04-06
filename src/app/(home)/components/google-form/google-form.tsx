4779a53f8a92ef3979faa2397ee7e10671bf25f4
nick20name17 <nick20name17@gmail.com>
2025-04-07 00:39:34 +0300

    fixes

--- src/app/(home)/components/google-form/google-form.tsx
+++ src/app/(home)/components/google-form/google-form.tsx
@@ -1,22 +1,23 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
 export const GoogleForm = () => {
   return (
     <section
-      className='container mt-20'
+      className='container mt-20 border-b pb-5'
       id='form'
     >
-      <h2 className='border-b pb-5 text-3xl leading-none font-semibold tracking-tight transition-colors'>
+      <h2 className='text-3xl leading-none font-semibold tracking-tight transition-colors'>
         Google form
       </h2>
-      <iframe
-        className='w-full rounded-xl'
-        src='https://docs.google.com/forms/d/e/1FAIpQLSduL8Q-9lqPZABGqkyguIpw8zsd2_aijjP7e4kcCactxvE4bw/viewform?embedded=true'
-        height='466'
-        frameBorder='0'
-        marginHeight={0}
-        marginWidth={0}
+      <Link
+        className='mt-3 block'
+        href='https://docs.google.com/forms/d/e/1FAIpQLSduL8Q-9lqPZABGqkyguIpw8zsd2_aijjP7e4kcCactxvE4bw/viewform?usp=sharing'
+        target='_blank'
       >
-        Завантаження…
-      </iframe>
+        <Button>Leave a complaint</Button>
+      </Link>
     </section>
   )
 }

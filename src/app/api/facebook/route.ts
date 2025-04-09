d013c8c7c32772e95a469235edbc3e7a61e0e1b2
nick20name17 <nick20name17@gmail.com>
2025-04-09 15:34:47 +0300

    update facebook env

--- src/app/api/facebook/route.ts
+++ src/app/api/facebook/route.ts
@@ -4,9 +4,8 @@ export async function POST(req: Request) {
   const body = await req.json()
   const { message } = body
 
-  const PAGE_ID = '122144807354605872'
-  const ACCESS_TOKEN =
-    'EAAPiTHc8AG0BOzBFLb52tNAGFYsEzJIIczvYViuFddhqXtSgw4aWckiy8Ii5ZBc4ZBZAiPcZAWPHL4mvc2f6zunYT3ezIjIrZBsW7SwgX3g6PphhHTUcDhV2hsfSk8ambOzjGI0vawTzUZCNyR5wrXVlZChIn2xJp2tXqlfry33NRvkMZAGpCTduriLK7j6nUGHdAetejzuZBMvYUGd6m4V7fSqpEa0YIVlDp'
+  const PAGE_ID = process.env.FACEBOOK_PAGE_ID
+  const ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN
 
   try {
     const res = await fetch(`https://graph.facebook.com/${PAGE_ID}/feed`, {
